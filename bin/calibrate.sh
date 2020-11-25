89190649b38ecb5e34ff89d7f9be3c01670e4595
steveprabu <steverajprabu@gmail.com>
2020-11-25 12:09:10 +0800

    first version of pipelien for continuous obs

---
+++ bin/calibrate.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -l
+#SBATCH --export=NONE
+#SBATCH -p workq
+#SBATCH --time=5:00:00
+#SBATCH --ntasks=28
+#SBATCH --mem=124GB
+#SBATCH -J calibration
+#SBATCH --mail-type FAIL,TIME_LIMIT,TIME_LIMIT_90
+#SBATCH --mail-user sirmcmissile47@gmail.com
+
+start=`date +%s`
+module load singularity
+shopt -s expand_aliases
+source /astro/mwasci/sprabu/aliases
+
+set -x
+{
+
+obsnum=OBSNUM
+base=BASE
+model=
+link=
+datadir=${base}processing
+cd ${datadir}
+
+rm -r ${obsnum}
+mkdir -p ${obsnum}
+cd  ${obsnum}
+
+
+while getopts 'm:l:' OPTION
+do
+    case "$OPTION" in
+        m)
+            model=${OPTARG}
+            ;;
+	l)
+	    link=${OPTARG}
+	    ;;
+    esac
+done
+
+
+## download file
+wget -O ${obsnum}_ms.zip --no-check-certificate "${link}"
+
+unzip -n ${obsnum}_ms.zip
+
+## flag bad tiles
+cp /home/sprabu/customPython/getFlaggedTiles.py .
+flagged=$(myPython ./getFlaggedTiles.py --metafits ${obsnum}.metafits)
+
+if [[ ! -z ${flagged} ]]; then
+    echo ${flagged} | xargs flagantennae ${obsnum}.ms
+fi
+
+## run aoflagger
+aoflagger ${obsnum}.ms
+
+## calibrate
+calibrate -absmem 120 -m ../../models/model-${model}*withalpha.txt -minuv 150 -ch 4 -applybeam -mwa-path /pawsey/mwa ${obsnum}.ms round1.bin
+
+## applysolution
+applysolutions ${obsnum}.ms round1.bin
+
+## flag the new column
+aoflagger -column CORRECTED_DATA ${obsnum}.ms
+
+## image for selfcal
+wsclean -name selfcal1 -size 1400 1400 -scale 1amin -niter 10000 -mgain 0.8 -weight natural -auto-threshold 1.5 -quiet ${obsnum}.ms
+
+## selfcal
+calibrate -absmem 120 -minuv 150 -ch 4 -applybeam -mwa-path /pawsey/mwa -quiet ${obsnum}.ms round2.bin
+
+applysolutions ${obsnum}.ms round2.bin
+wsclean -name selfcal2 -size 1400 1400 -scale 1amin -niter 10000 -mgain 0.8 -weight natural -auto-threshold 1.5 -quiet ${obsnum}.ms
+
+
+
+## interpolate for flagged freq
+cp /home/sprabu/customPython/interpolate .
+myPython ./interpolate --inputFile round2.bin --outputFile ${obsnum}.bin
+
+
+end=`date +%s`
+runtime=$((end-start))
+echo "the job run time ${runtime}"
+
+
+}
+
---
+++ bin/obs_calibrate.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+usage()
+{
+echo "calibrate.sh [-o obsnum] [-m model] [-d wget link] [-a account] [-m machine]
+	-o obsnum	: observation id
+	-m model	: the calibrator model
+	-a account	: the pawsey account.default=mwasci
+	-m machine	: the cluster to run the job.default=garrawarla" 1>&2;
+exit 1;
+}
+
+obsnum=
+model=
+account="mwasci"
+link=
+machine="garrawarla"
+while getopts 'o:m:a:m:' OPTION
+do
+    case "$OPTION" in
+        o)
+            obsnum=${OPTARG}
+            ;;
+        m)
+            model=${OPTARG}
+            ;;
+        d)
+            link=${OPTARG}
+            ;;
+        a)
+            account=${OPTARG}
+            ;;
+        m)
+            machine=${OPTARG}
+            ;;
+        ? | : | h)
+            usage
+            ;;
+    esac
+done
+
+
+
+
+# if obsid is empty then just pring help
+if [[ -z ${obsnum} ]]
+then
+    usage
+fi
+
+base=/astro/mwasci/sprabu/satellites/MWA-calibration/
+
+script="${base}queue/calibrate_${obsnum}.sh"
+cat ${base}bin/calibrate.sh | sed -e "s:OBSNUM:${obsnum}:g" \
+                                -e "s:BASE:${base}:g" > ${script}
+output="${base}queue/logs/calibrate_${obsnum}.o%A"
+error="${base}queue/logs/calibrate_${obsnum}.e%A"
+sub="sbatch --begin=now+15 --output=${output} --error=${error} -M ${machine} -A ${account} ${script} -m ${model} -l ${link}" 
+jobid=($(${sub}))
+jobid=${jobid[3]}
+
+# rename the err/output files as we now know the jobid
+error=`echo ${error} | sed "s/%A/${jobid}/"`
+output=`echo ${output} | sed "s/%A/${jobid}/"`
+
+echo "Submitted calibration job as ${jobid}"
+
+
+
